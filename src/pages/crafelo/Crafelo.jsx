5fb65ee0e45f780140acb99e54db6cb749b50420
akindoju <davidd.akindoju@gmail.com>
2022-01-07 15:51:50 +0100

    Created nav bar

--- src/pages/crafelo/Crafelo.jsx
+++ src/pages/crafelo/Crafelo.jsx
@@ -1,7 +1,12 @@
 import CrafeloNav from "./CrafeloNav/CrafeloNav";
+import "./Crafelo.scss";
 
 const Crafelo = () => {
-  return <CrafeloNav />;
+  return (
+    <div className="crafelo">
+      <CrafeloNav />
+    </div>
+  );
 };
 
 export default Crafelo;
