8a02f966450137c8a7b4dc9612ead0ceca6aca7b
akindoju <davidd.akindoju@gmail.com>
2022-01-08 18:27:55 +0100

    Created grid section

--- src/pages/crafelo/CrafeloBody/CrafeloBody.jsx
+++ src/pages/crafelo/CrafeloBody/CrafeloBody.jsx
@@ -1,11 +1,18 @@
 import "./CrafeloBody.scss";
+import img1 from "../images/img1.png";
+import img2 from "../images/img2.png";
+import img3 from "../images/img3.png";
+import img4 from "../images/img4.png";
+import img5 from "../images/img5.png";
+import img6 from "../images/img6.png";
 
 const CrafeloBody = () => {
   return (
     <div className="crafeloBody">
       <div className="crafeloBody__left">
         <h1>
-          Become a product <span>designer</span> in 6 weeks
+          Become a product <span>designer</span>
+          <br /> in 6 weeks
         </h1>
 
         <h4>
@@ -24,7 +31,24 @@ const CrafeloBody = () => {
       </div>
 
       <div className="crafeloBody__right">
-        <div>Sola</div>
+        <div className="crafeloBody__right--1 crafeloBody__right--div">
+          <img src={img1} alt="img1" />
+        </div>
+        <div className="crafeloBody__right--2 crafeloBody__right--div">
+          <img src={img2} alt="img2" />
+        </div>
+        <div className="crafeloBody__right--3 crafeloBody__right--div">
+          <img src={img3} alt="img3" />
+        </div>
+        <div className="crafeloBody__right--4 crafeloBody__right--div">
+          <img src={img4} alt="img4" />
+        </div>
+        <div className="crafeloBody__right--5 crafeloBody__right--div">
+          <img src={img5} alt="img5" />
+        </div>
+        <div className="crafeloBody__right--6">
+          <img src={img6} alt="img6" />
+        </div>
       </div>
     </div>
   );
